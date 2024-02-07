0c68466e6fb97d893af2f4353fbab63173a299d4
Mateusz <mateusz.laczny@interia.pl>
2024-02-07 19:20:45 +0100

    Initialize project

---
+++ src/app/bpmn-editor.js
@@ -0,0 +1,45 @@
+'use client';
+
+import BpmnModeler from 'bpmn-js/lib/Modeler';
+
+import React, { useEffect, useRef } from 'react';
+
+async function openDiagram(modeler, xml) {
+  try {
+    await modeler.importXML(xml);
+
+    container.removeClass('with-error').addClass('with-diagram');
+  } catch (err) {
+    container.removeClass('with-diagram').addClass('with-error');
+
+    container.find('.error pre').text(err.message);
+
+    console.error(err);
+  }
+}
+
+export default function ReactEditor() {
+  const containerRef = useRef(null);
+  const editorRef = useRef(null);
+
+  useEffect(() => {
+    if (containerRef !== null) {
+      const container = containerRef.current;
+      const bpmnEditor = new BpmnModeler({ container });
+      editorRef.current = bpmnEditor;
+    }
+  }, []);
+
+  useEffect(
+    () => () => {
+      if (editorRef.current !== null) {
+        editorRef.current.destroy();
+      }
+    },
+    [editorRef]
+  );
+
+  return (
+    <div style={{ width: '1500px', height: '1000px' }} ref={containerRef}></div>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import BpmnEditor from './bpmn-editor';
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <BpmnEditor />
+      </div>
+    </main>
+  );
+}
